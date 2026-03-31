dcddca7955f1e25dba2b02d5b266815fad6d84ad
boxtersh <boxtersh@yandex.ru>
2026-03-31 17:53:44 +0300

    fix

--- weather.js
+++ weather.js
@@ -1,10 +1,12 @@
 const btn = document.querySelector('.btn-dark');
 btn.addEventListener('click', () => {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Volgograd&appid=46c7c2167acec7df7f595a6906d77796')
-//        if (!response.ok) {console.log('Ошибка получения данных');}
-//        else {
+        if (!response.ok) {
+            console.log('Ошибка данных пользователя');
+        }
+        else {
             .then(response => response.json())
             .then(data => {console.log('Данные:', data);})
-//        }
-        .catch(error => {console.log(error);})
+        }
+        .catch(error => {console.log('Ошибка данных сервера:', error);})
 });
